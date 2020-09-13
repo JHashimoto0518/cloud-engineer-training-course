64369e6c1f0e9e828e0860ce7d8d1441606cf4d0
Junichi Hashimoto <junichi_hashimoto@hotmail.co.jp>
2020-09-13 12:16:25 +0900

    automated script generation

---
+++ scripts/generate_script.sh
@@ -0,0 +1,15 @@
+// read md
+private const string MdPath = @"../contents/day-02/create_vpc.md";
+var lines = File.ReadAllLines(MdPath);
+Debug.Assert(lines[0] == "");
+
+// select command lines (starting "$ ")
+var commandLines = lines.Where(l => l.StartsWith("$")).Select(l => l.Trim('$').Trim());
+Debug.Assert(commandLines.FirstOrDefault() == "");
+
+
+// to script file
+// TODO:consider newline 
+const string ScriptPath = "./day-02.sh";
+File.Delete(ScriptPath);
+File.AppendAllLines(ScriptPath, commandLines);
